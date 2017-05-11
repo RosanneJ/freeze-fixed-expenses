5c6b909a1aec551bbfcfc96b04c161551485c0d4
jooster <rosanne.joosten@rabobank.nl>
2017-05-11 19:17:41 +0200

    a transaction is returned to fe

--- backend/transaction-service/src/main/java/hello/Transaction.java
+++ backend/transaction-service/src/main/java/hello/Transaction.java
@@ -4,8 +4,12 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 @Builder
+@Getter
+@Setter
 public class Transaction {
     private long id;
     private BigDecimal amount;
--- backend/transaction-service/src/main/java/hello/TransactionRestController.java
+++ backend/transaction-service/src/main/java/hello/TransactionRestController.java
@@ -3,25 +3,26 @@ package hello;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TransactionRestController {
 
     @RequestMapping("/transactions")
+    @ResponseBody
     public List<Transaction> retrieveTransactions(@RequestParam(value = "name", defaultValue = "World") String name) {
         List<Transaction> transactions = new ArrayList<>();
         transactions.add(Transaction.builder()//
             .id(654564L)//
-            .amount(new BigDecimal(4))//
+            .amount(new BigDecimal(4.25))//
             .date(LocalDate.now())//
+            .incassantId("98753")//
             .build());
-        return Collections.emptyList();
+        return transactions;
     }
 }
